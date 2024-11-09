ef7e25383a4bf8299be3aac182b394ba24410c09
kevinfrias051@gmail.com <kevinfrias051@gmail.com>
2024-11-09 13:09:08 -0300

    config

--- src/services/axios/config.ts
+++ src/services/axios/config.ts
@@ -4,7 +4,6 @@ export const baseURL = 'http://ec2-54-145-211-254.compute-1.amazonaws.com:3000';
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL,
-  timeout: 60000,
   headers: {
     'Content-Type': 'multipart/form-data',
   },
